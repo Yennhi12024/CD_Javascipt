6b97d9fe237fbf9ddfeabd64dd91799ecded66d8
NguyenThiYenNhi <yennhiii12092004@gmail.com>
2024-05-16 15:25:33 +0700

    Nop bai buoi 4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,428 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import Logo from "./assets/images/logo1.webp";
+import Slider1 from "./assets/images/slider_1.webp";
+import Slider2 from "./assets/images/slider_1.webp";
 
+
+import { BsSearchHeart } from "react-icons/bs";
+import { IoBagRemove } from "react-icons/io5";
+import {
+  FaUserEdit,
+  FaCartPlus,
+  FaYoutube,
+  FaGoogle,
+  FaFacebook,
+  FaInstagram,
+} from "react-icons/fa";
+import { FaBell } from "react-icons/fa";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="bg-white">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3">
+              <img src={Logo} alt="logo" />
+            </div>
+            <div className="col-md-7">
+              <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                  <a class="navbar-brand" href="#">
+                    Navbar
+                  </a>
+                  <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                  >
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div
+                    class="collapse navbar-collapse"
+                    id="navbarSupportedContent"
+                  >
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">
+                          Home
+                        </a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">
+                          Link
+                        </a>
+                      </li>
+                      <li class="nav-item dropdown">
+                        <a
+                          class="nav-link dropdown-toggle"
+                          href="#"
+                          role="button"
+                          data-bs-toggle="dropdown"
+                          aria-expanded="false"
+                        >
+                          Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                          <li>
+                            <a class="dropdown-item" href="#">
+                              Action
+                            </a>
+                          </li>
+                          <li>
+                            <a class="dropdown-item" href="#">
+                              Another action
+                            </a>
+                          </li>
+                          <li>
+                            <hr class="dropdown-divider" />
+                          </li>
+                          <li>
+                            <a class="dropdown-item" href="#">
+                              Something else here
+                            </a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link disabled" aria-disabled="true">
+                          Disabled
+                        </a>
+                      </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                      <input
+                        class="form-control me-2"
+                        type="search"
+                        placeholder="Search"
+                        aria-label="Search"
+                      />
+                      <button class="btn btn-outline-success" type="submit">
+                        Search
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              </nav>
+            </div>
+            <div className="col-md-2">
+              <BsSearchHeart />
+              <IoBagRemove />
+              <FaUserEdit />
+              <FaCartPlus />
+              <FaBell />
+            </div>
+          </div>
+        </div>
       </header>
-    </div>
+      <main>
+        <div id="carouselExample" class="carousel slide">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src={Slider1} class="d-block w-100" alt="..." />
+            </div>
+            <div class="carousel-item">
+              <img src={Slider2} class="d-block w-100" alt="..." />
+            </div>
+            <div class="carousel-item">
+              <img src={Slider1} class="d-block w-100" alt="..." />
+            </div>
+          </div>
+          <button
+            class="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExample"
+            data-bs-slide="prev"
+          >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button
+            class="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExample"
+            data-bs-slide="next"
+          >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+
+        <div className="slider">
+          <div id="carouselExample" class="carousel slide">
+            
+            <button
+              class="carousel-control-prev"
+              type="button"
+              data-bs-target="#carouselExample"
+              data-bs-slide="prev"
+            >
+              <span
+                class="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button
+              class="carousel-control-next"
+              type="button"
+              data-bs-target="#carouselExample"
+              data-bs-slide="next"
+            >
+              <span
+                class="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+        <div>
+          <h1 class="text-center mt-3">New Product</h1>
+          <div class="row">
+            <div class="col-md-3">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Tên sản phẩm</h5>
+                  <p class="card-text">Mô tả sản phẩm ngắn gọn.</p>
+                  <a href="#" class="btn btn-primary">
+                    Mua ngay
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Tên sản phẩm</h5>
+                  <p class="card-text">Mô tả sản phẩm ngắn gọn.</p>
+                  <a href="#" class="btn btn-primary">
+                    Mua ngay
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Tên sản phẩm</h5>
+                  <p class="card-text">Mô tả sản phẩm ngắn gọn.</p>
+                  <a href="#" class="btn btn-primary">
+                    Mua ngay
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Tên sản phẩm</h5>
+                  <p class="card-text">Mô tả sản phẩm ngắn gọn.</p>
+                  <a href="#" class="btn btn-primary">
+                    Mua ngay
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row mt-4">
+            <div class="col-md-3">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Tên sản phẩm</h5>
+                  <p class="card-text">Mô tả sản phẩm ngắn gọn.</p>
+                  <a href="#" class="btn btn-primary">
+                    Mua ngay
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Tên sản phẩm</h5>
+                  <p class="card-text">Mô tả sản phẩm ngắn gọn.</p>
+                  <a href="#" class="btn btn-primary">
+                    Mua ngay
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Tên sản phẩm</h5>
+                  <p class="card-text">Mô tả sản phẩm ngắn gọn.</p>
+                  <a href="#" class="btn btn-primary">
+                    Mua ngay
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Tên sản phẩm</h5>
+                  <p class="card-text">Mô tả sản phẩm ngắn gọn.</p>
+                  <a href="#" class="btn btn-primary">
+                    Mua ngay
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h1 class="text-center mt-3">Sản phẩm bán chạy</h1>
+          <div class="row">
+            <div class="col-md-3">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Tên sản phẩm</h5>
+                  <p class="card-text">Mô tả sản phẩm ngắn gọn.</p>
+                  <a href="#" class="btn btn-primary">
+                    Mua ngay
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Tên sản phẩm</h5>
+                  <p class="card-text">Mô tả sản phẩm ngắn gọn.</p>
+                  <a href="#" class="btn btn-primary">
+                    Mua ngay
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Tên sản phẩm</h5>
+                  <p class="card-text">Mô tả sản phẩm ngắn gọn.</p>
+                  <a href="#" class="btn btn-primary">
+                    Mua ngay
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Tên sản phẩm</h5>
+                  <p class="card-text">Mô tả sản phẩm ngắn gọn.</p>
+                  <a href="#" class="btn btn-primary">
+                    Mua ngay
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row mt-4">
+            <div class="col-md-3">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Tên sản phẩm</h5>
+                  <p class="card-text">Mô tả sản phẩm ngắn gọn.</p>
+                  <a href="#" class="btn btn-primary">
+                    Mua ngay
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Tên sản phẩm</h5>
+                  <p class="card-text">Mô tả sản phẩm ngắn gọn.</p>
+                  <a href="#" class="btn btn-primary">
+                    Mua ngay
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Tên sản phẩm</h5>
+                  <p class="card-text">Mô tả sản phẩm ngắn gọn.</p>
+                  <a href="#" class="btn btn-primary">
+                    Mua ngay
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Tên sản phẩm</h5>
+                  <p class="card-text">Mô tả sản phẩm ngắn gọn.</p>
+                  <a href="#" class="btn btn-primary">
+                    Mua ngay
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer className="bg-light">
+        <section class="hdl-footer pb-4">
+          <div class="footer_container">
+            <div class="row">
+              <div class="col-md-4 pt-4">
+                <h3 class="widgettilte">CHÚNG TÔI LÀ AI ?</h3>
+                <p class="pt-1">
+                  <i>
+                    {" "}
+                    Copyright@ CELAH Shop là hệ thống cửa hàng mỹ phẩm chính
+                    hãng và dịch vụ chăm sóc sắc đẹp chuyên sâu với hệ thống cửa
+                    hàng trải dài trên toàn quốc; và hiện đang là đối tác phân
+                    phối chiến lược tại thị trường Việt Nam.
+                  </i>
+                </p>
+                <p class="pt-1">
+                  Địa chỉ: 79, ĐS 6, P.Phước Long B ,Q9,TP. Thủ Đức
+                </p>
+                <p class="pt-1">
+                  Điện thoại: 0375450149 (call, zalo) - Email:
+                  yennhiii12092004@gmail.com
+                </p>
+                <h3 class="widgettilte">MẠNG XÃ HỘI</h3>
+                <div class="social my-3">
+                  <FaFacebook />
+                  <FaInstagram />
+                  <FaGoogle />
+                  <FaYoutube />
+                </div>
+              </div>
+              <div class="col-md-4 pt-4">
+                <h3 class="widgettilte">
+                  <strong>Liên hệ</strong>
+                </h3>
+              </div>
+              <div class="col-md-4 pt-4 text-end">
+                <h3 class="fs-5 text-end">
+                  <strong>Lượt truy cập</strong>
+                </h3>
+                <p class="my-1">9999 lượt</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="dhl-copyright bg-dark py-3">
+          <div class="container text-center text-white">
+            Thiết kế bởi:Nguyen Thi Yen Nhi- Phone:0375450149
+          </div>
+        </section>
+      </footer>
+    </>
   );
 }
 
